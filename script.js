08011cb79c89239676bf8ee54430c6334d5a31fd
smunozc <salvador.munoz0109@gmail.com>
2020-11-10 11:12:40 +0100

    1 file modified

--- script.js
+++ script.js
@@ -16,8 +16,52 @@
 // Utilizad constantes siempre que sea posible.
 // Suerte!
 
+
+var users = [];
+
+function User (name,country,money,premiumAccount){
+  this.name = name;
+  this.country = country;
+  this.money = money;
+  this.premiumAccount = premiumAccount
+}
+
+function createUsers(){
+  let user1 = new User("usuario1", "spain", 199, true);
+  let user2 = new User("usuario2", "france", 0, false);
+  let user3 = new User("usuario3", "spain", 537, false);
+  let user4 = new User("usuario4", "italy", 1004, true);
+  let user5 = new User("usuario5", "spain", 250, false);
+  let user6 = new User("usuario6", "ireland", 799, true);
+  let user7 = new User("usuario7", "spain", 3345, false);
+
+  users.push(user1);
+  users.push(user2);
+  users.push(user3);
+  users.push(user4);
+  users.push(user5);
+  users.push(user6);
+  users.push(user7);
+
+}
+
+function filterUsers(array){
+  const money = 200;
+  let newArray = [];
+  for (let i = 0; i < array.length; i++){
+    if(array[i].money > money){
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
+
+createUsers();
+
 window.addEventListener('load',onLoad);
 
 function onLoad(){
+  console.log(users);
+  console.log(filterUsers(users));
   console.log('hi')
 }
